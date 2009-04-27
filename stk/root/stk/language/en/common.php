9ef485ae6707532ca9b3ed408c173b1cde0afb57
Erik Frèrejean <erikfrerejean@phpbb.com>
2009-04-27 11:48:13 +0000

    Display a notice when the user uses the build  in login method. We strongly advice to use the phpBB authentication method unless this isn't possible.

--- stk/root/stk/language/en/common.php
+++ stk/root/stk/language/en/common.php
@@ -38,6 +38,15 @@ if (empty($lang) || !is_array($lang))
 //
 
 $lang = array_merge($lang, array(
+	'BACK_TOOL'							=> 'Back to last Tool',
+	'BOARD_FOUNDER_ONLY'				=> 'Only Board Founders may access the Support Tool Kit.',
+
+	'INCORRECT_CLASS'					=> 'Incorrect class in: stk/tools/%1$s.%2$s',
+	'INCORRECT_PASSWORD'				=> 'Password is incorrect',
+	'INCORRECT_PHPBB_VERSION'			=> 'Your version of phpBB isn\'t compatible with this tool. Make sure that you are running phpBB version: %1$s, in order to run this tool',
+
+	'NOTICE'							=> 'Notice',
+
 	'SUPPORT_TOOL_KIT'					=> 'Support Tool Kit',
 	'SUPPORT_TOOL_KIT_INDEX'			=> 'Support Tool Kit Index',
 	'SUPPORT_TOOL_KIT_PASSWORD'			=> 'Password',
@@ -46,15 +55,10 @@ $lang = array_merge($lang, array(
 	'STK_LOGOUT'						=> 'STK Logout',
 	'STK_LOGOUT_SUCCESS'				=> 'You have successfully logged out from the Support Tool Kit.',
 
-	'BACK_TOOL'							=> 'Back to last Tool',
-	'BOARD_FOUNDER_ONLY'				=> 'Only Board Founders may access the Support Tool Kit.',
-
-	'INCORRECT_CLASS'					=> 'Incorrect class in: stk/tools/%1$s.%2$s',
-	'INCORRECT_PASSWORD'				=> 'Password is incorrect',
-	'INCORRECT_PHPBB_VERSION'			=> 'Your version of phpBB isn\'t compatible with this tool. Make sure that you are running phpBB version: %1$s, in order to run this tool',
-
 	'TOOL_INCLUTION_NOT_FOUND'			=> 'This tool is trying to load a not existing file: %1$s',
 	'TOOL_NAME'							=> 'Tool Name',
+
+	'USING_STK_LOGIN'					=> 'You are loged in using the internal STK authentication method. It is adviced to <strong>only</strong> use this method when you are unable to login to phpBB.<br />To turn this authentication method of empty the <code>$stk_password</code> value in <em>[' . STK_ROOT_PATH . 'config.' . PHP_EXT . ']</em>',
 
 	'WELCOME'							=> 'Support Tool Kit',
 	'WELCOME_EXPLAIN'					=> 'The Support Tool Kit, or STK, is a package that can be used to recover phpBB 3.0.x installations, or fix common issues within a working installation. The package is a second ACP that can be easily installed on any working phpBB 3 board, and has the look and feel of the phpBB 3 ACP, but gives the administrator access to a whole new set of tools that can be used in cases where phpBB might not function correctly anymore.',
