5bc0d408ca2220df4a03357d4537cc2885d8dd0d
Erik Frèrejean <erikfrerejean@phpbb.com>
2009-06-16 15:21:39 +0000

    cross commit

--- stk/root/stk/language/en/tools/sanitise_anonymous_user.php
+++ stk/root/stk/language/en/tools/sanitise_anonymous_user.php
@@ -38,14 +38,14 @@ if (empty($lang) || !is_array($lang))
 //
 
 $lang = array_merge($lang, array(
-	'ANONYMOUS_CLEANED'					=> 'The Anonymous user\'s profile data has been successfully sanitised.',
+	'ANONYMOUS_CLEANED'					=> 'The Anonymous user’s profile data has been successfully sanitised.',
 	'ANONYMOUS_CREATED'					=> 'The Anonymous user has been successfully re-created.',
 	'ANONYMOUS_CREATION_FAILED'			=> 'It was not possible to recreate the Anonymous user. Please ask for further assistance in the phpBB.com Support Forum.',
 	'ANONYMOUS_GROUPS_REMOVED'			=> 'The Anonymous user was successfully removed from all access groups.',
 	'ANONYMOUS_MISSING'					=> 'The Anonymous user is missing.',
 	'ANONYMOUS_MISSING_CONFIRM'			=> 'The anonymous user is missing in your database. This user is used to allow guests to visit your board. Do you want to create a new one?',
-	'ANONYMOUS_WRONG_DATA'				=> 'The Anonymous user\'s profile data is incorrect.',
-	'ANONYMOUS_WRONG_DATA_CONFIRM'		=> 'The Anonymous user\'s profile data is partially incorrect. Would you like to repair this?',
+	'ANONYMOUS_WRONG_DATA'				=> 'The Anonymous user’s profile data is incorrect.',
+	'ANONYMOUS_WRONG_DATA_CONFIRM'		=> 'The Anonymous user’s profile data is partially incorrect. Would you like to repair this?',
 	'ANONYMOUS_WRONG_GROUPS'			=> 'The Anonymous user improperly belongs to multiple user groups.',
 	'ANONYMOUS_WRONG_GROUPS_CONFIRM'	=> 'The Anonymous user improperly belongs to multiple user groups. Would you like to remove the Anonymous user from all but the "GUESTS" group?',
 
