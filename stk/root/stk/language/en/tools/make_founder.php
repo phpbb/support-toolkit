ba532a90db316a91063b5eda7f176c03d5a2448b
Steve Atkinson <stevemaury@phpbb.com>
2009-06-16 14:33:44 +0000

    Added this - This will give <a href="%1$s">%2$s</a> the ability to delete your account, among other powers. - to ensure the user understands the possible consequences.

--- stk/root/stk/language/en/tools/make_founder.php
+++ stk/root/stk/language/en/tools/make_founder.php
@@ -39,13 +39,13 @@ if (empty($lang) || !is_array($lang))
 
 $lang = array_merge($lang, array(
 	'MAKE_FOUNDER'				=> 'Make a user Board Founder',
-	'MAKE_FOUNDER_CONFIRM'		=> 'Are you sure you want to make <a href="%1$s">%2$s</a> a Board Founder?',
+	'MAKE_FOUNDER_CONFIRM'		=> 'Are you sure you want to make <a href="%1$s">%2$s</a> a Board Founder?  This will give <a href="%1$s">%2$s</a> the ability to delete your account, among other powers.',
 	'MAKE_FOUNDER_EXPLAIN'		=> 'Here you can change a user\'s type to be that of a board founder which has FULL administrative permissions.',
 	'MAKE_FOUNDER_SUCCESS'		=> 'Successfully made <a href="%1$s">%2$s</a> a Board Founder.',
 
 	'USER_ALREADY_FOUNDER'		=> '%s is already a Board Founder.',
 	'USER_TO_FOUNDER'			=> 'User to make Founder',
-	'USER_TO_FOUNDER_EXPLAIN'	=> 'Enter the username or user_id of the user you would like to make a Board Founder.',
+	'USER_TO_FOUNDER_EXPLAIN'	=> 'Enter the username or user ID of the user you would like to make a Board Founder.',
 ));
 
 ?>
