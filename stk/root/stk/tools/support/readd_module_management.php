835863ab3de758ac67f55d077c44b567a42b10f6
Erik Frèrejean <erikfrerejean@phpbb.com>
2009-08-28 16:41:09 +0000

    readd_module_management now also re-enables the modules if required

--- stk/root/stk/tools/support/readd_module_management.php
+++ stk/root/stk/tools/support/readd_module_management.php
@@ -19,6 +19,50 @@ if (!defined('IN_PHPBB'))
 class readd_module_management
 {
 	/**
+	 * The modules this class will check and re-add and/or enable if required.
+	 * The array contains all information we feed into $umil->module_exists and $umil->module_add
+	 */
+	var $check_modules = array(
+		array(
+			'class' 	=> 'acp',
+			'parent'	=> 0,
+			'lang_name'	=> 'ACP_CAT_SYSTEM'
+		),
+		array(
+			'class'		=> 'acp',
+			'parent'	=> 'ACP_CAT_SYSTEM',
+			'lang_name'	=> 'ACP_MODULE_MANAGEMENT'
+		),
+		array(
+			'class'		=> 'acp',
+			'parent'	=> 'ACP_MODULE_MANAGEMENT',
+			'lang_name'	=> 'ACP',
+			'data'		=> array(
+				'module_basename'	=> 'modules',
+				'modes'				=> array('acp'),
+			),
+		),
+		array(
+			'class'		=> 'acp',
+			'parent'	=> 'ACP_MODULE_MANAGEMENT',
+			'lang_name'	=> 'MCP',
+			'data'		=> array(
+				'module_basename'	=> 'modules',
+				'modes'				=> array('mcp'),
+			),
+		),
+		array(
+			'class'		=> 'acp',
+			'parent'	=> 'ACP_MODULE_MANAGEMENT',
+			'lang_name'	=> 'UCP',
+			'data'		=> array(
+				'module_basename'	=> 'modules',
+				'modes'				=> array('ucp'),
+			),
+		),
+	);
+	
+	/**
 	* Display Options
 	*
 	* Output the options available
@@ -35,38 +79,37 @@ class readd_module_management
 	*/
 	function run_tool()
 	{
-		global $umil;
-
-		if (!$umil->module_exists('acp', 0, 'ACP_CAT_SYSTEM'))
+		global $db, $umil;
+		
+		// Check all modules for existance
+		foreach ($this->check_modules as $module_data)
 		{
-			$umil->module_add('acp', 0, 'ACP_CAT_SYSTEM');
+			if(!$umil->module_exists($module_data['class'], $module_data['parent'], $module_data['lang_name']))
+			{
+				$umil->module_add($module_data['class'], $module_data['parent'], ((empty($module_data['data'])) ? $module_data['lang_name'] : $module_data['data']));
+			}
+			
+			// This module exists, now make sure that it is enabled
+			$sql = 'SELECT module_id
+				FROM ' . MODULES_TABLE . "
+				WHERE module_class = '" . $module_data['class'] . "'
+					AND module_langname = '" . $module_data['lang_name'] . "'
+					AND module_enabled = 1";
+			$result		= $db->sql_query_limit($sql, 1, 0);
+			$enabled	= $db->sql_fetchfield('module_id', false, $result);
+			$db->sql_freeresult($result);
+			
+			if (!$enabled)
+			{
+				// Enable it
+				$sql = 'UPDATE ' . MODULES_TABLE . "
+					SET module_enabled = 1
+					WHERE module_class = '" . $module_data['class'] . "'
+						AND module_langname = '" . $module_data['lang_name'] . "'";
+				$db->sql_query($sql);
+			}
 		}
-
-		if (!$umil->module_exists('acp', 'ACP_CAT_SYSTEM', 'ACP_MODULE_MANAGEMENT'))
-		{
-			$umil->module_add('acp', 'ACP_CAT_SYSTEM', 'ACP_MODULE_MANAGEMENT');
-		}
-
-		$to_add = array();
-
-		if (!$umil->module_exists('acp', 'ACP_MODULE_MANAGEMENT', 'ACP'))
-		{
-			$to_add[] = 'acp';
-		}
-		if (!$umil->module_exists('acp', 'ACP_MODULE_MANAGEMENT', 'MCP'))
-		{
-			$to_add[] = 'mcp';
-		}
-		if (!$umil->module_exists('acp', 'ACP_MODULE_MANAGEMENT', 'UCP'))
-		{
-			$to_add[] = 'ucp';
-		}
-
-		$umil->module_add('acp', 'ACP_MODULE_MANAGEMENT', array(
-			'module_basename'	=> 'modules',
-			'modes'				=> $to_add,
-		));
-
+		
 		trigger_error('READD_MODULE_MANAGEMENT_SUCCESS');
 	}
 }
