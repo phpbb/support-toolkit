d344b785569ed0ea6e81b8fb5bcb2371fc1c59f1
Erik Frèrejean <erikfrerejean@phpbb.com>
2009-09-04 15:54:11 +0000

    Keyword

--- stk/root/stk/includes/database_cleaner/3_0_6.php
+++ stk/root/stk/includes/database_cleaner/3_0_6.php
@@ -2,7 +2,7 @@
 /**
 *
 * @package Support Toolkit - Database Cleaner
-* @version $Id:$
+* @version $Id$
 * @copyright (c) 2009 phpBB Group
 * @license http://opensource.org/licenses/gpl-license.php GNU Public License
 *
