8966a42ff3ab1a7ea60f81b0eb21911d9810eeb4
Phillip Crumm <phil@phpbb.com>
2010-01-19 23:16:23 +0000

    #56865 - Note to translators: Language string verbiage was corrected but overall meaning was not changed. You should review the changes and determine whether or not your translation requires modification.

--- stk/language/en/common.php
+++ stk/language/en/common.php
@@ -42,15 +42,15 @@ $lang = array_merge($lang, array(
    'BOARD_FOUNDER_ONLY'					=> 'Only Board Founders may access the Support Toolkit.',
 
    'CAT_ADMIN'							=> 'Admin Tools',
-   'CAT_ADMIN_EXPLAIN'					=> 'The tools within this category are tools that can be used by the administrator to perform tasks that don’t ship with a vanilla phpBB 3.0.x installation.',
+   'CAT_ADMIN_EXPLAIN'					=> 'Administrative Tools may be used by an administrator to manage particular aspects of their forum and solve common problems.',
    'CAT_DEV'							=> 'Developer Tools',
-   'CAT_DEV_EXPLAIN'					=> 'The "developer tools" are tools that can be used by phpBB developers.',
+   'CAT_DEV_EXPLAIN'					=> 'Developer Tools may be used by phpBB Developers and MODders to perform common tasks.',
    'CAT_MAIN'							=> 'Main',
-   'CAT_MAIN_EXPLAIN'					=> 'The Support Toolkit, or STK, is a package that can be used to recover phpBB 3.0.x installations, or fix common issues within a working installation. The package is a second ACP that can be easily installed on any working phpBB 3 board, and has the look and feel of the phpBB 3 ACP, but gives the administrator access to a whole new set of tools that can be used in cases where phpBB might not function correctly anymore.',
+   'CAT_MAIN_EXPLAIN'					=> 'The Support Toolkit (STK) may be used to fix common issues within a working installation of phpBB 3.0.x. It serves as a second Administration Control Panel, providing an administrator with a set of tools to resolve common problems that may prevent a phpBB3 installation from functioning properly.',
    'CAT_SUPPORT'						=> 'Support Tools',
-   'CAT_SUPPORT_EXPLAIN'				=> 'The support tools are tools that are used to fix or recover certain parts of a phpBB 3.0.x installation.',
+   'CAT_SUPPORT_EXPLAIN'				=> 'Support Tools may be used to aid in the recovery of a phpBB 3.0.x installation that is no longer functioning properly.',
    'CAT_USER_GROUP'						=> 'User/Group Tools',
-   'CAT_USER_GROUP_EXPLAIN'				=> 'This category contains tools that are focused on users and groups',
+   'CAT_USER_GROUP_EXPLAIN'				=> 'User and Group Tools may be used to manage users and groups in ways that are not available in a stock phpBB 3.0.x installation.',
    'CONFIG_NOT_FOUND'					=> 'The STK configuration file couldn’t be loaded. Please check your installation',
 
    'DOWNLOAD_PASS'						=> 'Download the password file.',
@@ -59,20 +59,20 @@ $lang = array_merge($lang, array(
 
    'FAIL_REMOVE_PASSWD'					=> 'Couldn’t remove the expired password file. Please remove this file manually!',
 
-   'GEN_PASS_FAILED'					=> 'Something went wrong during the generation of the password file. Please re-generate a new one.',
+   'GEN_PASS_FAILED'					=> 'The Support Toolkit was unable to generate a new password. Please try again.',
    'GEN_PASS_FILE'						=> 'Generate password file.',
    'GEN_PASS_FILE_EXPLAIN'				=> 'If you aren’t able to login to phpBB at all you can use the internal authentication method of the Support Toolkit. To use this method you must <a href="%s"><strong>generate</strong></a> a new password file.',
 
    'INCORRECT_CLASS'					=> 'Incorrect class in: stk/tools/%1$s.%2$s',
    'INCORRECT_PASSWORD'					=> 'Password is incorrect',
-   'INCORRECT_PHPBB_VERSION'			=> 'Your version of phpBB isn’t compatible with this tool. Make sure that you are running phpBB version: %1$s, in order to run this tool',
+   'INCORRECT_PHPBB_VERSION'			=> 'Your version of phpBB isn’t compatible with this tool. Your phpBB installation must be version %1$s or later in order to run this tool',
 
    'LOGIN_STK_SUCCESS'					=> 'You have successfully authenticated and will now be redirected to the Support Toolkit.',
 
    'NOTICE'								=> 'Notice',
-   'NO_VERSION_FILE'					=> 'The Support Toolkit coudn’t determine the latest version of the package. Please go the Support Toolkit <a href="http://www.phpbb.com/support/stk/">pages</a> and make sure that you are using the latest version of the package before continueing using it.',
+   'NO_VERSION_FILE'					=> 'The Support Toolkit (STK) was not able to determine the latest version. Please go to the <a href="http://phpbb.com/support/stk">Support Toolkit section of phpBB.com</a> and verify that you are using the latest version before using the STK.',
 
-   'PASS_GENERATED'						=> 'Your STK password file was successfully generated!<br/>The password that was generated for you is: <em>%1$s</em><br />This password will expire on: <span style="text-decoration: underline;">%2$s</span>, after this time you <strong>must</strong> generate a new file in order to keep using the emergency login feature!<br /><br />Use the following button to download the file. Once you’ve downloaded this file you must upload it to your server into the "stk" directory',
+   'PASS_GENERATED'						=> 'Your STK password file was successfully generated!<br/>The password that was generated for you is: <em>%1$s</em><br />This password will expire on: <span style="text-decoration: underline;">%2$s</span>. After this time you <strong>must</strong> generate a new password file in order to keep using the emergency login feature!<br /><br />Use the following button to download the file. Once you’ve downloaded this file you must upload it to your server into the "stk" directory',
    'PASS_GENERATED_REDIRECT'			=> 'Once you have uploaded the password file to the correct location, click <a href="%s">here</a> to go back to the login page.',
    'PLUGIN_INCOMPATIBLE_PHPBB_VERSION'	=> 'This tool isn’t compatible with the version of phpBB that you are running',
    'PROCEED_TO_STK'						=> '%sProceed to the Support Toolkit%s',
