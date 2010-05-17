b4701745575f1d748839eed6eb8f1075553957b4
Erik Frèrejean <erikfrerejean@phpbb.com>
2010-05-17 16:15:50 +0000

    Use an ignore list to define the dirs/files we dont want in the archives

--- stk/build/stkbuild.sh
+++ stk/build/stkbuild.sh
@@ -4,7 +4,7 @@
 # $Id$
 #
 # Settings
-revision="333"
+revision="379"
 version="1.0.0-pl2"
 
 # vars
@@ -22,13 +22,16 @@ svn export -qr "$revision" http://code.phpbb.com/svn/support-toolkit/"$repo"/ "$
 
 # Zip
 echo "Creating $file.zip"
-zip -qr "$file".zip "$file" -x "*develop*" "*build*"
+zip -qr "$file".zip "$file" --exclude @/tmp/"$file"/stk/build/exclude.lst
 
 # tar.bz2
 echo "Creating $file.tar.bz2"
-tar -cvjf "$file".tar.bz2 "$file" --exclude "develop" "build" 1> /dev/null 2>&1
+tar -cvjf "$file".tar.bz2 "$file" -X /tmp/"$file"/stk/build/exclude.lst 1> /dev/null 2>&1
 
 echo "Moving files to the Desktop"
 mv "$file".* ~/Desktop
+
+# Delete the export
+rm -rf /tmp/"$file"
 
 echo "Done!";
