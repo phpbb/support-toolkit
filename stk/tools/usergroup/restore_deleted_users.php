ce1d3f106240ef49709cf432de9ac79af7ebff49
Erik Frèrejean <erikfrerejean@phpbb.com>
2010-05-31 11:18:37 +0000

    First draft for #56465, multibyte usernames and names that already have an account dont work yet

---
+++ stk/tools/usergroup/restore_deleted_users.php
@@ -0,0 +1,167 @@
+<?php
+/**
+*
+* @package Support Toolkit - Restore Deleted Users
+* @version $Id$
+* @copyright (c) 2009 phpBB Group
+* @license http://opensource.org/licenses/gpl-license.php GNU Public License
+*
+*/
+
+/**
+* @ignore
+*/
+if (!defined('IN_PHPBB'))
+{
+	exit;
+}
+
+class restore_deleted_users
+{
+	/**
+	* Display Options
+	*
+	* Output the options available
+	*/
+	function display_options()
+	{
+		global $db;
+
+		// Find all guest posters
+		$sql = 'SELECT DISTINCT post_username
+			FROM ' . POSTS_TABLE . '
+			WHERE poster_id = ' . ANONYMOUS;
+		$result	= $db->sql_query($sql);
+		$users	= $db->sql_fetchrowset($result);
+		$db->sql_freeresult($result);
+
+		// Build the output
+		$user_vars = array();
+		foreach ($users as $cnt => $user)
+		{
+			$user_vars["user[{$user['post_username']}]"] = array('lang' => $user['post_username'], 'explain' => false, 'type' => 'checkbox');
+		}
+
+		// Return usable data
+		return array(
+			'title'	=> 'RESTORE_DELETED_USERS',
+			'vars'	=> array_merge(array(
+				'legend1'	=> 'SELECT_USERS',
+			), $user_vars),
+		);
+	}
+
+	/**
+	* Run Tool
+	*
+	* Does the actual stuff we want the tool to do after submission
+	*/
+	function run_tool(&$error)
+	{
+		global $config, $db, $umil;
+
+		if (!check_form_key('restore_deleted_users'))
+		{
+			$error[] = 'FORM_INVALID';
+			return;
+		}
+
+		// Get the selected users
+		$selected = array_keys(request_var('user', array('' => '')));
+		if (empty($selected))
+		{
+			$error[] = 'NO_USER_SELECTED';
+			return;
+		}
+
+		// Conflicted names are users that already exist
+		$selected_clean = $selected;
+		array_walk($selected_clean, 'utf8_clean_string');
+		$conflicted = array();
+		$sql = 'SELECT username
+			FROM ' . USERS_TABLE . '
+			WHERE ' . $db->sql_in_set('username_clean', $selected_clean);
+		$result = $db->sql_query($sql);
+		while ($row = $db->sql_fetchrow($result))
+		{
+			$conflicted[] = $row;
+		}
+		$db->sql_query($result);
+
+		// Go through the non-users and add them
+		$non_conflicted = array_diff($selected, $conflicted);
+
+		if (!function_exists('user_add'))
+		{
+			include PHPBB_ROOT_PATH . 'includes/functions_user.' . PHP_EXT;
+		}
+
+		// Registered users group?
+		$sql = 'SELECT group_id, group_colour
+			FROM ' . GROUPS_TABLE . "
+			WHERE group_name = 'REGISTERED'";
+		$result = $db->sql_query_limit($sql, 1);
+		$gid	= $db->sql_fetchfield('group_id', false, $result);
+		$gcl	= $db->sql_fetchfield('group_colour', 0, $result);
+		$db->sql_freeresult($result);
+
+		foreach ($non_conflicted as $user)
+		{
+			// Setup the user
+			$user_ary = array(
+				'username'		=> $user,
+				'group_id'		=> $gid,
+				'user_email'	=> $config['board_email'],	// Use the board email
+				'user_type'		=> USER_NORMAL,
+				'user_password'	=> gen_rand_string(12),
+			);
+
+			// Add the user
+			$user_id = user_add($user_ary);
+
+			// Update posts table
+			$sql = 'UPDATE ' . POSTS_TABLE . "
+				SET poster_id = {$user_id}, post_username = ''
+				WHERE post_username = '{$user}'";
+			$db->sql_query($sql);
+
+			// Update topics table (first post)
+			$sql = 'UPDATE ' . TOPICS_TABLE . "
+				SET topic_poster = {$user_id}, topic_first_poster_colour = '{$gcl}'
+				WHERE topic_first_poster_name = '{$user}'";
+			$db->sql_query($sql);
+
+			// Update topics table (last post)
+			$sql = 'UPDATE ' . TOPICS_TABLE . "
+				SET topic_last_poster_id = {$user_id}, topic_last_poster_colour = '{$gcl}'
+				WHERE topic_last_poster_name = '{$user}'";
+			$db->sql_query($sql);
+
+			// Update forums table
+			$sql = 'UPDATE ' . FORUMS_TABLE . "
+				SET forum_last_poster_id = {$user_id}, forum_last_poster_colour = '{$gcl}'
+				WHERE forum_last_poster_name = '{$user}'";
+			$db->sql_query($sql);
+
+			// Update user post count
+			$sql = 'SELECT COUNT(post_id) as post_cnt
+				FROM ' . POSTS_TABLE . '
+				WHERE poster_id = ' . $user_id;
+			$result		= $db->sql_query($sql);
+			$post_cnt	= $db->sql_fetchfield('post_cnt', false, $result);
+			$db->sql_freeresult($result);
+
+			$sql = 'UPDATE ' . USERS_TABLE . ' SET user_posts = ' . $post_cnt . ' WHERE user_id = ' . $user_id;
+			$db->sql_query($sql);
+		}
+
+		// Clear the cache
+		$umil->cache_purge(array(
+			array('auth'),
+			array('data'),
+		));
+
+		trigger_error(((sizeof($non_conflicted) > 1) ? 'USERS_RESTORED_SUCCESSFULLY' : 'USER_RESTORED_SUCCESSFULLY'));
+	}
+}
+?>
