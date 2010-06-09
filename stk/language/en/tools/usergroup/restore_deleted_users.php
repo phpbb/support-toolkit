fca47fb952f3ab13d89ada9e9e218817cf26b63a
Phillip Crumm <phil@phpbb.com>
2010-06-09 00:49:36 +0000

    Oops, forgot one =/

--- stk/language/en/tools/usergroup/restore_deleted_users.php
+++ stk/language/en/tools/usergroup/restore_deleted_users.php
@@ -40,10 +40,10 @@ if (empty($lang) || !is_array($lang))
 $lang = array_merge($lang, array(
 	'NO_USER_SELECTED'	=> 'No users selected!',
 
-	'RESTORE_DELETED_USERS'						=> 'Restore deleted users',
-	'RESTORE_DELETED_USERS_CONFLICT'			=> 'Restore deleted users :: Conflicted',
-	'RESTORE_DELETED_USERS_CONFLICT_EXPLAIN'	=> 'This tool allows you to restore users that are deleted from the board but still have "guest" posts on the board.<br />These users will be assigned a random password that you after the tool is finished have to reset manually. This tool doesn\'t provide a list of generated passwords per user!<br /><br />During the last run this tool found some usernames that already exist on this board. Please provide a new name for these users.',
-	'RESTORE_DELETED_USERS_EXPLAIN'				=> 'This tool allows you to restore users that are deleted from the board but still have "guest" posts on the board.<br />These users will be assigned a random password that you after the tool is finished have to reset manually. This tool doesn\'t provide a list of generated passwords per user!',
+	'RESTORE_DELETED_USERS'						=> 'Restore Deleted Users',
+	'RESTORE_DELETED_USERS_CONFLICT'			=> 'Restore Deleted Users :: Conflicted',
+	'RESTORE_DELETED_USERS_CONFLICT_EXPLAIN'	=> 'This tool allows you to restore users that are deleted from the board but still have "guest" posts on the board.<br />These users will be assigned a random password that you must reset manually after the tool has ran. This tool doesn\'t provide a list of generated passwords per user!<br /><br />During the last run this tool found some usernames that already exist on this board. Please provide a new name for these users.',
+	'RESTORE_DELETED_USERS_EXPLAIN'				=> 'This tool allows you to restore users that are deleted from the board but still have "guest" posts on the board.<br />These users will be assigned a random password that you must reset manually after the tool has ran. This tool doesn\'t provide a list of generated passwords per user!',
 
 	'SELECT_USERS'	=> 'Select users to restore',
 
