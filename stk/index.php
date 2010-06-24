643b92679693dd45f8e1c867e14ced16f0e0199a
Erik Frèrejean <erikfrerejean@phpbb.com>
2010-06-24 13:30:22 +0000

    Use DIRECTORY_SEPARATOR

--- stk/index.php
+++ stk/index.php
@@ -21,7 +21,7 @@ define('IN_LOGIN', true);
 
 if (!defined('PHPBB_ROOT_PATH')) { define('PHPBB_ROOT_PATH', './../'); }
 if (!defined('PHP_EXT')) { define('PHP_EXT', substr(strrchr(__FILE__, '.'), 1)); }
-if (!defined('STK_DIR_NAME')) { define('STK_DIR_NAME', substr(strrchr(dirname(__FILE__), '/'), 1)); }	// Get the name of the stk directory
+if (!defined('STK_DIR_NAME')) { define('STK_DIR_NAME', substr(strrchr(dirname(__FILE__), DIRECTORY_SEPARATOR), 1)); }	// Get the name of the stk directory
 if (!defined('STK_ROOT_PATH')) { define('STK_ROOT_PATH', './'); }
 if (!defined('STK_INDEX')) { define('STK_INDEX', STK_ROOT_PATH . 'index.' . PHP_EXT); }
 
