98e0fa206b1b35ce400d51bdb55a0abfd8ce7291
Erik Frèrejean <erikfrerejean@phpbb.com>
2010-07-05 09:58:57 +0000

    #62132

--- stk/tools/support/auto_cookies.php
+++ stk/tools/support/auto_cookies.php
@@ -29,11 +29,15 @@ class auto_cookies
 
 		$user->add_lang('acp/board');
 
+		// Remove "www". Bug #62132
+		$_domain = ((!empty($_SERVER['HTTP_HOST'])) ? htmlspecialchars(strtolower($_SERVER['HTTP_HOST']), ENT_COMPAT, 'UTF-8') : ((!empty($_SERVER['SERVER_NAME'])) ? htmlspecialchars($_SERVER['SERVER_NAME'], ENT_COMPAT, 'UTF-8') : htmlspecialchars(getenv('SERVER_NAME'), ENT_COMPAT, 'UTF-8')));
+		$_domain = (strpos($_domain, 'www') === 0) ? substr($_domain, 3) : $_domain;
+
 		return array(
 			'title'	=> 'AUTO_COOKIES',
 			'vars'	=> array(
 				'legend1'				=> 'AUTO_COOKIES',
-				'cookie_domain'			=> array('lang' => 'COOKIE_DOMAIN', 'type' => 'text:40:255', 'explain' => false, 'default' => ((!empty($_SERVER['HTTP_HOST'])) ? htmlspecialchars(strtolower($_SERVER['HTTP_HOST']), ENT_COMPAT, 'UTF-8') : ((!empty($_SERVER['SERVER_NAME'])) ? htmlspecialchars($_SERVER['SERVER_NAME'], ENT_COMPAT, 'UTF-8') : htmlspecialchars(getenv('SERVER_NAME'), ENT_COMPAT, 'UTF-8')))),
+				'cookie_domain'			=> array('lang' => 'COOKIE_DOMAIN', 'type' => 'text:40:255', 'explain' => false, 'default' => $_domain),
 				'cookie_name'			=> array('lang' => 'COOKIE_NAME', 'type' => 'text:40:255', 'explain' => false, 'default' => $config['cookie_name']),
 				'cookie_path'			=> array('lang' => 'COOKIE_PATH', 'type' => 'text:40:255', 'explain' => false, 'default' => htmlspecialchars(substr($_SERVER['PHP_SELF'], 0, -13)), ENT_COMPAT, 'UTF-8'),
 				'cookie_secure'			=> array('lang' => 'COOKIE_SECURE', 'type' => 'radio:disabled_enabled', 'explain' => true, 'default' => ((isset($_SERVER['HTTPS'])) ? true : false)),
