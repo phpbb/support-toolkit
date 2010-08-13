2884c1448be8306fb91252c5df5075d95ecb9c3b
Chris Smith <toonarmy@phpbb.com>
2010-08-13 07:37:47 +0000

    Add WHERE clause to SQL queries generated when merging private messages. #62247

--- stk/tools/usergroup/merge_users.php
+++ stk/tools/usergroup/merge_users.php
@@ -801,8 +801,9 @@ class merge_users
 			$sql[] = 'UPDATE ' . PRIVMSGS_TABLE . '
 				SET ' . $db->sql_build_array('UPDATE', array(
 					'to_address'	=> implode(':', array_unique(explode(':', str_replace('u_' . $source['user_id'], 'u_' . $target['user_id'], $row['to_address'])))),
-					'bcc_address'	=> implode(':', array_unique(explode(':', str_replace('u_' . $source['user_id'], 'u_' . $target['user_id'], $row['bcc_address'])))),
-			));
+					'bcc_address'	=> implode(':', array_unique(explode(':', str_replace('u_' . $source['user_id'], 'u_' . $target['user_id'], $row['bcc_address']))))
+			)) . '
+			WHERE msg_id = ' . (int) $row['msg_id'];
 		}
 
 		return $sql;
