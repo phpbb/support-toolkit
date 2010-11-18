589597babb546f3a1f62735fdd10748ceda92177
Phil Crumm <pcrumm@p3net.net>
2010-11-17 18:08:46 -0800

    Up version number

--- stk/common.php
+++ stk/common.php
@@ -17,7 +17,7 @@ if (!defined('IN_PHPBB'))
 }
 
 // What version are we using?
-define('STK_VERSION', '1.0.2');
+define('STK_VERSION', '1.0.2.1');
 //define('STK_QA', true);
 
 define('ADMIN_START', true);
