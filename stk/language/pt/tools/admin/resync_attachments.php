8fd9d38ff239b75746fec03c8f0af1387df16cd9
Marcus Vinny <vinnykun@hotmail.com>
2011-02-16 21:17:24 -0300

    Portuguese(pt) Translation

---
+++ stk/language/pt/tools/admin/resync_attachments.php
@@ -0,0 +1,44 @@
+<?php
+/**
+ *
+ * @package Support Toolkit - Resync Attachments
+ * @copyright (c) 2009 phpBB Group
+ * @license http://opensource.org/licenses/gpl-license.php GNU Public License
+* @Traduzido por: http://phpbbportugal.com - segundo as normas do Acordo Ortográfico
+ */
+
+/**
+ * DO NOT CHANGE
+ */
+if (!defined('IN_PHPBB'))
+{
+	exit;
+}
+
+if (empty($lang) || !is_array($lang))
+{
+	$lang = array();
+}
+
+// DEVELOPERS PLEASE NOTE
+//
+// All language files should use UTF-8 as their encoding and the files must not contain a BOM.
+//
+// Placeholders can now contain order information, e.g. instead of
+// 'Page %s of %s' you can (and should) write 'Page %1$s of %2$s', this allows
+// translators to re-order the output of data while ensuring it remains correct
+//
+// You do not need this where single placeholders are used, e.g. 'Message %d' is fine
+// equally where a string contains only two placeholders which are used to wrap text
+// in a url you again do not need to specify an order e.g., 'Click %sHERE%s' is fine
+//
+// Some characters you may want to copy&paste:
+// ’ » “ ” …
+//
+
+$lang = array_merge($lang, array(
+	'RESYNC_ATTACHMENTS'			=> 'Sincronizar anexos novamente',
+	'RESYNC_ATTACHMENTS_CONFIRM'	=> 'Esta ferramenta verifica se todos os anexos armazenados na Base de Dados têm o respetivo ficheiro no Servidor. Se o ficheiro não existir, esta ferramenta irá remover o anexo da base de dados. Tem certeza que deseja continuar?',
+	'RESYNC_ATTACHMENTS_FINISHED'	=> 'Anexos sincronizados com sucesso!',
+	'RESYNC_ATTACHMENTS_PROGRESS'	=> 'Sincronização de anexos em andamento. Não interrompa este processo.',
+));
