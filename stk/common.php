96d47d81455dfd5ba69e0a1f7906d748415fca3c
Erik Frèrejean <erikfrerejean@phpbb.com>
2011-05-31 22:01:16 +0200

    [prep-release/1.0.4] Bump the version for RC2

--- stk/common.php
+++ stk/common.php
@@ -17,7 +17,7 @@ if (!defined('IN_PHPBB'))
 }
 
 // What version are we using?
-define('STK_VERSION', '1.0.4-rc1');
+define('STK_VERSION', '1.0.4-rc2');
 define('STK_QA', true);
 
 define('ADMIN_START', true);
