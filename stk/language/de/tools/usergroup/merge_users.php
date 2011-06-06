fc70f6a70bb07f98e1261f937fddb0f747496cbf
Ingo Migliarina <mungo@phpbb.de>
2011-06-06 23:10:05 +0200

    STK 1.0.4

---
+++ stk/language/de/tools/usergroup/merge_users.php
@@ -0,0 +1,62 @@
+<?php
+/**
+*
+* @package Support Toolkit - Merge Users
+* @version $Id: merge_users.php 544 2011-01-30 16:52:26Z philippk $
+* @author Chris Smith <toonarmy@phpbb.com> (http://www.cs278.org/)
+* @copyright (c) 2009 phpBB Group
+* @license http://opensource.org/licenses/gpl-license.php GNU Public License
+*
+* Deutsche Übersetzung durch die Übersetzer-Gruppe von phpBB.de:
+* siehe docs/AUTHORS und http://www.phpbb.de/go/ubersetzerteam
+*
+*/
+
+/**
+* DO NOT CHANGE
+*/
+if (!defined('IN_PHPBB'))
+{
+	exit;
+}
+
+if (empty($lang) || !is_array($lang))
+{
+	$lang = array();
+}
+
+// DEVELOPERS PLEASE NOTE
+//
+// All language files should use UTF-8 as their encoding and the files must not contain a BOM.
+//
+// Placeholders can now contain order information, e.g. instead of
+// 'Page %s of %s' you can (and should) write 'Page %1$s of %2$s', this allows
+// translators to re-order the output of data while ensuring it remains correct
+//
+// You do not need this where single placeholders are used, e.g. 'Message %d' is fine
+// equally where a string contains only two placeholders which are used to wrap text
+// in a url you again do not need to specify an order e.g., 'Click %sHERE%s' is fine
+//
+// Some characters you may want to copy&paste:
+// ’ » “ ” …
+//
+
+$lang = array_merge($lang, array(
+	'MERGE_USERS'						=> 'Benutzer zusammenführen',
+	'MERGE_USERS_EXPLAIN'				=> 'Tool, um die Elemente eines Benutzers auf einen anderen Benutzer zu übertragen; die Einstellungen und Gruppenmitgliedschaften des Ursprungs-Benutzers werden kopiert. Die übertragenen Elemente umfassen benutzerspezifische Berechtigungen, Dateianhänge, Sperren, Lesezeichen, Entwürfe, Benachrichtigungen, ungelesene Beiträge, Log-Einträge, Abstimmungen, Beiträge, Private Nachrichten, Meldungen, Themen, Verwarnungen, Freunde und ignorierte Benutzer.',
+
+	'MERGE_USERS_BOTH_FOUNDERS'	=> 'Du kannst keinen Gründer mit einem Nicht-Gründer zusammenführen.',
+	'MERGE_USERS_BOTH_IGNORE'	=> 'Du kannst keinen Bot mit einem normalen Benutzer zusammenfürhen.',
+
+	'MERGE_USERS_MERGED'		=> 'Benutzer erfolgreich zusammengeführt.',
+
+	'MERGE_USERS_REMOVE_SOURCE'			=> 'Ursprungs-Benutzer entfernen',
+	'MERGE_USERS_REMOVE_SOURCE_EXPLAIN'	=> 'Wenn aktiviert, wird der Ursprungs-Benutzer gelöscht.',
+
+	'MERGE_USERS_SAME_USERS'	=> 'Der Ursprungs- und der Ziel-Benutzer müssen unterschiedlich sein.',
+
+	'MERGE_USERS_USER_SOURCE'			=> 'Ursprungs-Benutzer',
+	'MERGE_USERS_USER_SOURCE_EXPLAIN'	=> 'Beiträge, Private Nachrichten, Berechtigungen, Verwarnungen usw. dieses Benutzers werden auf den Ziel-Benutzer übertragen; Gruppenmitgliedschaften und Benutzereinstellungen werden kopiert.',
+
+	'MERGE_USERS_USER_TARGET'	=> 'Ziel-Benutzer',
+));
---
+++ stk/language/de_x_sie/tools/usergroup/resync_user_groups.php
@@ -0,0 +1,56 @@
+<?php
+/**
+ *
+ * @package Support Toolkit - Resynchronise Registered users groups
+ * @copyright (c) 2009 phpBB Group
+ * @license http://opensource.org/licenses/gpl-license.php GNU Public License
+ *
+ * Deutsche Übersetzung durch die Übersetzer-Gruppe von phpBB.de:
+ * siehe docs/AUTHORS und http://www.phpbb.de/go/ubersetzerteam
+ */
+
+/**
+* DO NOT CHANGE
+*/
+if (!defined('IN_PHPBB'))
+{
+	exit;
+}
+
+if (empty($lang) || !is_array($lang))
+{
+	$lang = array();
+}
+
+// DEVELOPERS PLEASE NOTE
+//
+// All language files should use UTF-8 as their encoding and the files must not contain a BOM.
+//
+// Placeholders can now contain order information, e.g. instead of
+// 'Page %s of %s' you can (and should) write 'Page %1$s of %2$s', this allows
+// translators to re-order the output of data while ensuring it remains correct
+//
+// You do not need this where single placeholders are used, e.g. 'Message %d' is fine
+// equally where a string contains only two placeholders which are used to wrap text
+// in a url you again do not need to specify an order e.g., 'Click %sHERE%s' is fine
+//
+// Some characters you may want to copy&paste:
+// ’ » “ ” …
+//
+
+$lang = array_merge($lang, array(
+	'RESYNC_USER_GROUPS'			=> 'Benutzergruppen synchronisieren',
+	'RESYNC_USER_GROUPS_EXPLAIN'	=> 'Dieses Tool dient dazu, zu prüfen, ob alle Benutzer Mitglied der richtigen Standard-Gruppe sind. <em>(Registrierte Benutzer, Registrierte COPPA-Benutzer und Kürzlich registrierte Benutzer)</em>',
+	'RESYNC_USER_GROUPS_NO_RUN'		=> 'Alle Gruppen scheinen aktuell zu sein!',
+	'RESYNC_USER_GROUPS_SETTINGS'	=> 'Synchronisations-Optionen',
+	'RUN_BOTH_FINISHED'				=> 'Alle Benutzergruppen wurden erfolgreich synchronisert!',
+	'RUN_RNR'						=> 'Neu registrierte Benutzer synchronisieren',
+	'RUN_RNR_EXPLAIN'				=> 'Dadurch wird die Gruppe „Kürzlich registrierte Benutzer“ aktualisiert, so dass alle Benutzer dieser Gruppe zugeordnet sind, die den Kriterien entsprechen, die im Administrations-Bereich festgelegt wurden.',
+	'RUN_RNR_FINISHED'				=> 'Die Gruppe für kürzlich registrierte Benutzer wurde erfolgreich synchronisiert!',
+	'RUN_RNR_NOT_FINISHED'			=> 'Die Gruppe für kürzlich registrierte Benutzer wird synchronisiert. Bitte unterbrechen Sie diesen Prozess nicht.',
+	'RUN_RR'						=> 'Registrierte Benutzer synchronisieren',
+	'RUN_RR_EXPLAIN'				=> 'Das Tool hat festgestellt, dass nicht alle Benutzer des Boards Mitglied der Gruppe „Registrierte <em>(COPPA-)</em>Benutzer“ sind. Möchten Sie, dass diese Gruppen synchronisiert werden?<br /><strong>Hinweis:</strong> Wenn COPPA in Ihrem Board aktiviert ist und der Benutzer kein Geburtsdatum angegeben hat, so wird er der Gruppe „Registrierte COPPA-Benutzer“ hinzugefügt!',
+	'RUN_RR_FINISHED'				=> 'Die Benutzer wurden erfolgreich synchronisiert!',
+
+	'SELECT_RUN_GROUP'	=> 'Bitte wählen Sie mindestens eine Gruppe aus, die synchronisiert werden soll.',
+));
