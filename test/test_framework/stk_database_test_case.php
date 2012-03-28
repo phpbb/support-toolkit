4075ff69365b5d8fd34d96652fec621d9920ae35
Erik Frèrejean <erikfrerejean@phpbb.com>
2012-03-28 12:32:12 +0200

    [stk/ascraeus] Set the `stk_test_case_helpers` object when running "db tests"

--- test/test_framework/stk_database_test_case.php
+++ test/test_framework/stk_database_test_case.php
@@ -41,4 +41,14 @@ abstract class stk_database_test_case extends phpbb_database_test_case
 	{
 		return new stk_database_test_connection_manager($config);
 	}
+
+	public function get_test_case_helpers()
+	{
+		if (!$this->test_case_helpers)
+		{
+			$this->test_case_helpers = new stk_test_case_helpers($this);
+		}
+
+		return $this->test_case_helpers;
+	}
 }
