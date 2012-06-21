b51f7ac5a2c64636b0d3406e0dec137907013568
Erik Frèrejean <erikfrerejean@phpbb.com>
2012-06-21 13:11:47 +0200

    [prep-release/1.0.6] Remove unused `@version $Id$`

--- stk/includes/database_cleaner/functions_database_cleaner.php
+++ stk/includes/database_cleaner/functions_database_cleaner.php
@@ -2,7 +2,6 @@
 /**
  *
  * @package Support Toolkit - Database Cleaner
- * @version $Id$
  * @copyright (c) 2009 phpBB Group
  * @license http://opensource.org/licenses/gpl-license.php GNU Public License
  *
