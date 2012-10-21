cd17eef477adc4e3726bebd08de46bce2d629880
Patrick Webster <noxwizard@phpbb.com>
2012-10-20 20:58:26 -0500

    Add some missing periods.

--- stk/language/en/tools/support/resync_report_flags.php
+++ stk/language/en/tools/support/resync_report_flags.php
@@ -40,5 +40,5 @@ $lang = array_merge($lang, array(
 	'RESYNC_REPORT_FLAGS'			=> 'Resynchronise report flags',
 	'RESYNC_REPORT_FLAGS_CONFIRM'	=> 'This tool will resynchronise the report flags for all posts, topics and private messages.',
 	'RESYNC_REPORT_FLAGS_FINISHED'	=> 'All report flags have successfully been resynchronised!',
-	'RESYNC_REPORT_FLAGS_NEXT'		=> 'Resynchronising report flags in progress. Please do not interrupt this process',
+	'RESYNC_REPORT_FLAGS_NEXT'		=> 'Resynchronising report flags in progress. Please do not interrupt this process.',
 ));
--- stk/language/en/tools/usergroup/resync_user_groups.php
+++ stk/language/en/tools/usergroup/resync_user_groups.php
@@ -38,14 +38,14 @@ if (empty($lang) || !is_array($lang))
 
 $lang = array_merge($lang, array(
 	'RESYNC_USER_GROUPS'			=> 'Resynchronise user groups',
-	'RESYNC_USER_GROUPS_EXPLAIN'	=> 'This tool is designed to check whether all users are part of the correct default groups <em>(Registered Users, Registered COPPA Users and Newly Registered Users)</em>',
+	'RESYNC_USER_GROUPS_EXPLAIN'	=> 'This tool is designed to check whether all users are part of the correct default groups <em>(Registered Users, Registered COPPA Users and Newly Registered Users)</em>.',
 	'RESYNC_USER_GROUPS_NO_RUN'		=> 'All groups seem to be up to date!',
 	'RESYNC_USER_GROUPS_SETTINGS'	=> 'Resynchronise options',
 	'RUN_BOTH_FINISHED'				=> 'All user groups have been resynchronised successfully!',
 	'RUN_RNR'						=> 'Resynchronise newly registered users',
 	'RUN_RNR_EXPLAIN'				=> 'This will update the "Newly Registered Users" group so that it contains all users that fit the criteria specified in the ACP.',
 	'RUN_RNR_FINISHED'				=> 'The Newly Registered Users group was successfully resynchronised!',
-	'RUN_RNR_NOT_FINISHED'			=> 'The Newly Registered Users group is currently being resynchronised. Please don’t interrupt this process',
+	'RUN_RNR_NOT_FINISHED'			=> 'The Newly Registered Users group is currently being resynchronised. Please don’t interrupt this process.',
 	'RUN_RR'						=> 'Resynchronise registered users',
 	'RUN_RR_EXPLAIN'				=> 'The tool has determined that not all users on your board are part of the "Registered <em>(COPPA)</em> users" group. Do you want to resyncronise these groups?<br /><strong>Note:</strong> If your board has COPPA enabled an a user hasn\'t entered a date of birth the user will be placed in the "Registered COPPA users" group!',
 	'RUN_RR_FINISHED'				=> 'The users have been resynchronised successfully!',
