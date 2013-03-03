de232586d661d1bbead598f2034557902732b3ac
Patrick Webster <noxwizard@phpbb.com>
2013-03-02 20:50:08 -0600

    New tool for fixing orphaned posts and topics

---
+++ stk/language/en/tools/support/orphaned_posts.php
@@ -0,0 +1,67 @@
+<?php
+/**
+*
+* @package Support Toolkit - Orphaned posts/topics
+* @version $Id$
+* @copyright (c) 2009 phpBB Group
+* @license http://opensource.org/licenses/gpl-license.php GNU Public License
+*
+*/
+
+/**
+* DO NOT CHANGE
+*/
+if (!defined('IN_PHPBB'))
+{
+	exit;
+}
+
+if (empty($lang) || !is_array($lang))
+{
+	$lang = array();
+}
+
+// DEVELOPERS PLEASE NOTE
+//
+// All language files should use UTF-8 as their encoding and the files must not contain a BOM.
+//
+// Placeholders can now contain order information, e.g. instead of
+// 'Page %s of %s' you can (and should) write 'Page %1$s of %2$s', this allows
+// translators to re-order the output of data while ensuring it remains correct
+//
+// You do not need this where single placeholders are used, e.g. 'Message %d' is fine
+// equally where a string contains only two placeholders which are used to wrap text
+// in a url you again do not need to specify an order e.g., 'Click %sHERE%s' is fine
+//
+// Some characters you may want to copy&paste:
+// ’ » “ ” …
+//
+
+$lang = array_merge($lang, array(
+	'AUTHOR'					=> 'Author',
+	'FORUM_NAME'				=> 'Forum Name',
+	'NEW_TOPIC_ID'				=> 'New Topic ID',
+	'POST_ID'					=> 'Post ID',
+	'TOPIC_ID'					=> 'Topic ID',
+
+	'DELETE_EMPTY_TOPICS'		=> 'Delete all selected topics by clicking on this button. (Can’t be undone!)',
+	'EMPTY_TOPICS'				=> 'Empty Topics',
+	'EMPTY_TOPICS_EXPLAIN'		=> 'These are topics that have no posts associated with them.',
+	'NO_EMPTY_TOPICS'			=> 'No empty topics found',
+	'NO_TOPICS_SELECTED'		=> 'No topics selected',
+
+	'ORPHANED_POSTS'			=> 'Orphaned Posts',
+	'ORPHANED_POSTS_EXPLAIN'	=> 'These are posts that do not have a topic associated with them. Specify a new topic ID to have the post attached to that topic.',
+	'NO_ORPHANED_POSTS'			=> 'No orphaned posts found',
+	'NO_TOPIC_IDS'				=> 'No topic IDs provided',
+	'REASSIGN'					=> 'Reassign',
+
+	'DELETE_SHADOWS'			=> 'Delete all selected shadow topics by clicking on this button. (Can’t be undone!)',
+	'ORPHANED_SHADOWS'			=> 'Orphaned Shadow Topics',
+	'ORPHANED_SHADOWS_EXPLAIN'	=> 'These are shadow topics whose target topic no longer exists.',
+	'NO_ORPHANED_SHADOWS'		=> 'No orphaned shadow topics found',
+
+	'POSTS_DELETED'				=> '%d posts deleted',
+	'POSTS_REASSIGNED'			=> '%d posts re-assigned',
+	'TOPICS_DELETED'			=> '%d topics deleted',
+));
---
+++ stk/tools/support/orphaned_posts.php
@@ -0,0 +1,211 @@
+<?php
+/**
+*
+* @package Support Toolkit - Orphaned posts/topics
+* @version $Id$
+* @copyright (c) 2009 phpBB Group
+* @license http://opensource.org/licenses/gpl-license.php GNU Public License
+*
+*/
+
+/**
+* @ignore
+*/
+if (!defined('IN_PHPBB'))
+{
+	exit;
+}
+
+class orphaned_posts
+{
+	function display_options()
+	{
+		global $db, $template, $user, $phpbb_root_path, $phpEx;
+
+		//
+		// Empty topics
+		//
+		$sql = 'SELECT t.topic_id, t.topic_title, t.topic_poster, t.forum_id, count(p.post_id) as num_posts, f.forum_name, u.user_id, u.username, u.user_colour
+			FROM ' . TOPICS_TABLE . ' t
+			LEFT JOIN ' . POSTS_TABLE . ' p ON (p.topic_id = t.topic_id)
+			JOIN ' . USERS_TABLE . ' u ON (u.user_id = topic_poster)
+			JOIN ' . FORUMS_TABLE . ' f ON (f.forum_id = t.forum_id)
+			WHERE t.topic_moved_id = 0
+			GROUP BY t.topic_id
+			HAVING num_posts = 0';
+		$result = $db->sql_query($sql);
+
+		while ($row = $db->sql_fetchrow($result))
+		{
+			$template->assign_block_vars('topics', array(
+				'FORUM_ID'		=> $row['forum_id'],
+				'FORUM_NAME'	=> $row['forum_name'],
+				'U_FORUM'		=> append_sid("{$phpbb_root_path}viewforum.$phpEx", 'f=' . $row['forum_id']),
+				'TOPIC_ID'		=> $row['topic_id'],
+				'TOPIC_TITLE'	=> $row['topic_title'],
+				'USER_FULL'		=> get_username_string('full', $row['user_id'], $row['username'], $row['user_colour']),
+				'USER_ID'		=> $row['user_id'],
+			));
+		}
+		$db->sql_freeresult($result);
+
+		//
+		// Orphaned posts
+		//
+		$sql = 'SELECT post_id, post_time, post_username, post_subject, post_text, bbcode_uid, bbcode_bitfield, p.forum_id, f.forum_name, u.user_id, u.username, u.user_colour
+			FROM ' . POSTS_TABLE . ' p
+			JOIN ' . USERS_TABLE . ' u ON (u.user_id = p.poster_id)
+			JOIN ' . FORUMS_TABLE . ' f ON (f.forum_id = p.forum_id)
+			WHERE NOT EXISTS (SELECT topic_id FROM ' . TOPICS_TABLE . ' WHERE topic_id = p.topic_id)';
+		$result = $db->sql_query($sql);
+
+		while ($row = $db->sql_fetchrow($result))
+		{
+			$message = generate_text_for_display($row['post_text'], $row['bbcode_uid'], $row['bbcode_bitfield'], OPTION_FLAG_BBCODE | OPTION_FLAG_SMILIES);
+			$search_keywords = urlencode((strpos($row['post_subject'], 'Re: ') === 0) ? utf8_substr($row['post_subject'], 4) : $row['post_subject']);
+
+			$template->assign_block_vars('posts', array(
+				'FORUM_ID'		=> $row['forum_id'],
+				'FORUM_NAME'	=> $row['forum_name'],
+				'U_FORUM'		=> append_sid("{$phpbb_root_path}viewforum.$phpEx", 'f=' . $row['forum_id']),
+				'POST_ID'		=> $row['post_id'],
+				'POST_SUBJECT'	=> $row['post_subject'],
+				'POST_TEXT'		=> $message,
+				'SEARCH_URL'	=> append_sid("{$phpbb_root_path}search.$phpEx", 'keywords=' . $search_keywords . '&amp;terms=all&amp;sf=titleonly&amp;sr=topics&amp;submit=Search', true),
+				'USER_FULL'		=> get_username_string('full', $row['user_id'], $row['username'], $row['user_colour']),
+				'USER_ID'		=> $row['user_id'],
+			));
+		}
+		$db->sql_freeresult($result);
+
+		//
+		// Orphaned shadow topics
+		//
+		$sql = 'SELECT t.topic_id, t.topic_moved_id, t.topic_title, t.topic_poster, t.forum_id, f.forum_name, u.user_id, u.username, u.user_colour
+			FROM ' . TOPICS_TABLE . ' t
+			JOIN ' . USERS_TABLE . ' u ON (u.user_id = t.topic_poster)
+			JOIN ' . FORUMS_TABLE . ' f ON (f.forum_id = t.forum_id)
+			WHERE topic_moved_id <> 0 
+				AND NOT EXISTS (SELECT topic_id FROM ' . TOPICS_TABLE . ' WHERE topic_id = t.topic_moved_id)';
+		$result = $db->sql_query($sql);
+
+		while ($row = $db->sql_fetchrow($result))
+		{
+			$template->assign_block_vars('shadows', array(
+				'FORUM_ID'		=> $row['forum_id'],
+				'FORUM_NAME'	=> $row['forum_name'],
+				'U_FORUM'		=> append_sid("{$phpbb_root_path}viewforum.$phpEx", 'f=' . $row['forum_id']),
+				'TOPIC_ID'		=> $row['topic_id'],
+				'TOPIC_TITLE'	=> $row['topic_title'],
+				'USER_FULL'		=> get_username_string('full', $row['user_id'], $row['username'], $row['user_colour']),
+				'USER_ID'		=> $row['user_id'],
+			));
+		}
+		$db->sql_freeresult($result);
+
+		$template->assign_vars(array(
+			'U_EMPTY_TOPICS'	=> append_sid(STK_INDEX, array('c' => 'support', 't' => 'orphaned_posts', 'mode' => 'empty_topics')),
+			'U_ORPHANED_POSTS'	=> append_sid(STK_INDEX, array('c' => 'support', 't' => 'orphaned_posts', 'mode' => 'orphaned_posts', 'submit' => 1)),
+			'U_ORPHANED_SHADOWS'=> append_sid(STK_INDEX, array('c' => 'support', 't' => 'orphaned_posts', 'mode' => 'orphaned_shadows')),
+		));
+
+		$template->set_filenames(array(
+			'body' => 'tools/orphaned_posts.html',
+		));
+
+		page_header($user->lang['ORPHANED_POSTS'], false);
+		page_footer();
+	}
+
+	/**
+	* Perform the right actions
+	* @param Array $error An array that will be filled with error messages that might occure
+	* @return void
+	*/
+	function run_tool(&$error)
+	{
+		global $db, $user;
+
+		$user->add_lang('ucp');
+
+		if (!check_form_key('orphaned_posts'))
+		{
+			$error[] = 'FORM_INVALID';
+			return;
+		}
+
+		$mode = request_var('mode', '');
+		switch ($mode)
+		{
+			case 'empty_topics':
+			case 'orphaned_shadows':
+				$topic_ids = request_var('topics', array(0 => 0));
+				if (!sizeof($topic_ids))
+				{
+					trigger_error('NO_TOPICS_SELECTED');
+				}
+
+				if (!function_exists('delete_topics'))
+				{
+					include($phpbb_root_path . 'includes/functions_admin.' . $phpEx);
+				}
+
+				$return = delete_topics('topic_id', $topic_ids);
+				trigger_error(sprintf($user->lang['TOPICS_DELETED'], $return['topics']));
+			break;
+
+			case 'orphaned_posts':
+				if (isset($_POST['reassign']))
+				{
+					$post_map = request_var('posts', array(0 => 0));
+
+					foreach ($post_map as $post_id => $topic_id)
+					{
+						if ($topic_id == 0)
+						{
+							unset($post_map[$post_id]);
+						}
+					}
+
+					if (!sizeof($post_map))
+					{
+						trigger_error('NO_TOPIC_IDS');
+					}
+
+					foreach ($post_map as $post_id => $topic_id)
+					{
+						$sql = 'UPDATE ' . POSTS_TABLE . ' SET topic_id = ' . (int) $topic_id . ' WHERE post_id = ' . (int) $post_id;
+						$db->sql_query($sql);
+					}
+
+					trigger_error(sprintf($user->lang['POSTS_REASSIGNED'], sizeof($post_map)));
+				}
+				else if (isset($_POST['delete']))
+				{
+					$post_ids = request_var('posts_del', array(0 => 0));
+
+					if (!sizeof($post_ids))
+					{
+						trigger_error('NO_POSTS_SELECTED');
+					}
+
+					if (!function_exists('delete_posts'))
+					{
+						include($phpbb_root_path . 'includes/functions_admin.' . $phpEx);
+					}
+
+					$return = delete_posts('post_id', $post_ids);
+					trigger_error(sprintf($user->lang['POSTS_DELETED'], $return));
+				}
+				else
+				{
+					trigger_error('NO_MODE');
+				}
+			break;
+
+			default:
+				trigger_error('NO_MODE');
+			break;
+		}
+	}
+}
