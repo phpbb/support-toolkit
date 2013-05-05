6cbcc4249d7b99d523d55caa86d2901e73a5adbc
Patrick Webster <noxwizard@phpbb.com>
2013-05-04 23:34:57 -0500

    Update version to 1.0.8-dev

--- stk/common.php
+++ stk/common.php
@@ -16,8 +16,8 @@ if (!defined('IN_PHPBB'))
 }
 
 // What version are we using?
-define('STK_VERSION', '1.0.7');
-//define('STK_QA', true);
+define('STK_VERSION', '1.0.8-dev');
+define('STK_QA', true);
 
 define('ADMIN_START', true);
 
