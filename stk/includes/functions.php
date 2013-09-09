9d369a2e963a445958c5aa16c874308f33fa3b1d
Patrick Webster <noxwizard@phpbb.com>
2013-09-09 18:49:38 -0500

    Make the send_status_line() function backwards compatible.

--- stk/includes/functions.php
+++ stk/includes/functions.php
@@ -743,7 +743,7 @@ function stk_msg_handler($errno, $msg_text, $errfile, $errline)
 			}
 
 			// Do not send 200 OK, but service unavailable on errors
-			send_status_line(503, 'Service Unavailable');
+			stk_send_status_line(503, 'Service Unavailable');
 
 			garbage_collection();
 
@@ -814,7 +814,7 @@ function stk_msg_handler($errno, $msg_text, $errfile, $errline)
 
 			if ($msg_text == 'ERROR_NO_ATTACHMENT' || $msg_text == 'NO_FORUM' || $msg_text == 'NO_TOPIC' || $msg_text == 'NO_USER')
 			{
-				send_status_line(404, 'Not Found');
+				stk_send_status_line(404, 'Not Found');
 			}
 
 			$msg_text = (!empty($user->lang[$msg_text])) ? $user->lang[$msg_text] : $msg_text;
@@ -1009,4 +1009,45 @@ function stk_array_walk_keys(&$array, $callback)
 		unset($array[$key]);
 	}
 	$array = $tmp_array;
+}
+
+/**
+* Outputs correct status line header.
+*
+* Depending on php sapi one of the two following forms is used:
+*
+* Status: 404 Not Found
+*
+* HTTP/1.x 404 Not Found
+*
+* HTTP version is taken from HTTP_VERSION environment variable,
+* and defaults to 1.0.
+*
+* Sample usage:
+*
+* send_status_line(404, 'Not Found');
+*
+* @param int $code HTTP status code
+* @param string $message Message for the status code
+* @return void
+*/
+function stk_send_status_line($code, $message)
+{
+	if (substr(strtolower(@php_sapi_name()), 0, 3) === 'cgi')
+	{
+		// in theory, we shouldn't need that due to php doing it. Reality offers a differing opinion, though
+		header("Status: $code $message", true, $code);
+	}
+	else
+	{
+		if (!empty($_SERVER['SERVER_PROTOCOL']))
+		{
+			$version = $_SERVER['SERVER_PROTOCOL'];
+		}
+		else
+		{
+			$version = 'HTTP/1.0';
+		}
+		header("$version $code $message", true, $code);
+	}
 }
